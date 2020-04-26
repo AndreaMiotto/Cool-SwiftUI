7de268a0cb56ec3f03c2a0fe4bac1fe62ca4b7e1
Andrea Miotto <miotto.andrea@icloud.com>
2020-04-26 13:55:57 +1000

    Initial Commit

---
+++ Cool-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cool-SwiftUI
+//
+//  Created by Andrea Miotto on 26/4/20.
+//  Copyright © 2020 Andrea Miotto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
